b5730f77cb0f2395c7c2ad89f8a003460d345fc4
Byeong Min Cho <merrybmc@gmail.com>
2025-01-29 18:58:54 +0900

    GSAP React stagger 실습

--- gsap_react/src/router/Router.jsx
+++ gsap_react/src/router/Router.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import App1 from '../components/1-1.to/App';
 import App2 from '../components/1-2.from/App';
+import App3 from '../components/1-3.stagger/App';
 
 export default function router() {
   return (
     <Routes>
       <Route path='/to' element={<App1 />} />
       <Route path='/from' element={<App2 />} />
+      <Route path='/stagger' element={<App3 />} />
     </Routes>
   );
 }
