23f103c3f833e6465c1f7c7dfe3c188a0ff0067c
Byeong Min Cho <merrybmc@gmail.com>
2025-01-31 00:02:54 +0900

    GSAP html basic timeline 실습

---
+++ chapter1/3.timeline/script.js
@@ -0,0 +1,3 @@
+gsap.from('.sun', { duration: 1, opacity: 0, x: 50, y: 50 });
+gsap.from('.bird', { delay: 1, duration: 1, opacity: 0, y: 100 });
+gsap.from('.gress', { delay: 3, duration: 1, opacity: 0, y: 100 });
