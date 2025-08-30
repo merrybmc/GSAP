fd300f13fcfd35a158d09f1926a01cb53bceb5bd
Byeong Min Cho <merrybmc@gmail.com>
2025-08-30 19:56:43 +0900

    gsap 3-1 nontimeline

---
+++ chapter1/3.timeline/1.nonTimeline/script.js
@@ -0,0 +1,9 @@
+// .sun
+// .gress
+// .bird
+// .music
+
+gsap.from(".sun", { duration: 1, opacity: 0, x: 50, y: 50 });
+gsap.from(".gress", { delay: 1, duration: 1, opacity: 0, y: 100 });
+gsap.from(".bird", { delay: 2, duration: 1, opacity: 0, y: 100 });
+gsap.from(".music", { delay: 3, duration: 1, opacity: 0, x: 100, y: 100 });
